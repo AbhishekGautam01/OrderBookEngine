d55001b286a2cc6356a8566cf2eabbceeef3bc4b
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2022-06-23 00:32:43 +0530

    Added Server port configuration

--- OrderBookEngine/ServerConfiguration/OrderBookEngineServerConfiguration.cs
+++ OrderBookEngine/ServerConfiguration/OrderBookEngineServerConfiguration.cs
@@ -6,5 +6,11 @@ namespace OrderBookEngineServer.Core.ServerConfiguration
 {
     class OrderBookEngineServerConfiguration
     {
+        public OrderBookEngineServerSettings OrderBookEngineServerSettings { get; set; }
+    }
+
+    class OrderBookEngineServerSettings
+    {
+        public int Port { get; set; }
     }
 }
