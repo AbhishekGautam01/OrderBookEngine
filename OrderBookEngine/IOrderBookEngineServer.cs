bfc4d6cc578aaf8d70ba28c9cb82fc510dae0c21
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2022-06-23 00:05:29 +0530

    Added a Basic Skeleton for OrderBookEnginerServer Added Placeholder for Configuration for OrderBookEngineServer Added library for DI, Hosting , Logging

---
+++ OrderBookEngine/IOrderBookEngineServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OrderBookEngineServer.Core
+{
+    interface IOrderBookEngineServer
+    {
+        Task Run(CancellationToken token);
+    }
+}
---
+++ OrderBookEngine/OrderBookEngineServer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using OrderBookEngineServer.Core.ServerConfiguration;
+
+namespace OrderBookEngineServer.Core
+{
+    class OrderBookEngineServer : BackgroundService, IOrderBookEngineServer
+    {
+        private readonly ILogger<OrderBookEngineServer> _logger;
+        private readonly OrderBookEngineServerConfiguration _config;
+        public OrderBookEngineServer(ILogger<OrderBookEngineServer> logger, IOptions<OrderBookEngineServerConfiguration> config)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _config = config.Value ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        /// <summary>
+        /// This method is there to make the execute async as by default we have done a protected override of this ExecuteAsync but we want to call it from other places.
+        /// This will be used when we will not use microsoft hosting to call execute async for us. 
+        /// </summary>
+        /// <param name="token">Cancellation Token</param>
+        /// <returns></returns>
+        public Task Run(CancellationToken token) => ExecuteAsync(token);
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
