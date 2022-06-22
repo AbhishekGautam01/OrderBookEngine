faef7ff9dc4e3e4ad652d6ecb1dc1ce1638e9d0d
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2022-06-23 00:56:13 +0530

    Added a Logger type of define various types of logger available to our application

---
+++ Logging/Logging/LoggerType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderBookEngineServer.Logging
+{
+    public enum LoggerType
+    {
+        Text, 
+        Database, 
+        Trace, 
+        Console
+    }
+}
